92d411c4e4f4dcd7a29bddf0efeaf0a4a8463ff3
ysbc1247 <106061905+ysbc1247@users.noreply.github.com>
2022-07-11 23:04:26 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<iomanip>
+#include<map>
+#include<set>
+#include<queue>
+#include<functional>
+#include<regex>
+#include<stack>
+#include<deque>
+#include<list>
+#define endl "\n"
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+using namespace std;
+
+
+
+
+int main() {
+	fastio;
+	
+	int k;
+	cin >> k;
+	cout << 2000<<endl;
+	for (int i = 0; i < 1000; i++) {
+		cout << 1 << ' ';
+	}
+	for (int i = 0; i < 1000; i++) {
+		cout << 1000 << ' ';
+	}
+
+}
