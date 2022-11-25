9643e583e7a546f2798161c9e43496a3afb9d21d
ysbc1247 <106061905+ysbc1247@users.noreply.github.com>
2022-11-25 20:38:55 +0900

    [Platinum III] Title: 열혈강호 3, Time: 408 ms, Memory: 4164 KB -BaekjoonHub

---
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using ll = long long;
+#define all(v) v.begin(),v.end()
+using namespace std;
+
+
+const int MAXA = 1010, MAXB = 1010;
+int A[MAXA], B[MAXB];
+bool visited[MAXA];
+vector<int> adj[MAXA];
+int n, m, k;
+bool DFS(int here) {
+    if (visited[here]) return false;
+    visited[here] = true;
+    for (int& there : adj[here])
+        if (B[there] == -1 || DFS(B[there])) {
+            B[there] = here;
+            return true;
+        }
+    return false;
+}
+int biMatch(int a) {
+    
+    int match = 0;
+    for (int i = 0; i < MAXA; ++i)
+        if (A[i] == -1) {
+            memset(visited, 0, sizeof(visited));
+            if (DFS(i)) {
+                ++match;
+                if (a == 2) {
+                    k--; if (!k)break;
+                }
+            }
+        }
+    return match;
+}
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+   cin >> n >> m >> k;
+    for (int i = 0; i < n; i++) {
+        int a; cin >> a;
+        while (a--) {
+            int b; cin >> b;
+            adj[i].push_back(b);
+        }
+    }
+    memset(B, -1, sizeof(B));
+    memset(A, -1, sizeof(A));
+    int res = biMatch(1);
+    memset(A, -1, sizeof(A));
+    res += biMatch(2);
+    cout << res;
+}
+/*
+5 5 2
+3 1 2 3
+3 1 2 3
+1 5
+1 4
+1 5
+*/
